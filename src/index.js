445cdfc34c7609f866b2bcd1178290b61a855296
張天慈 <woodychang0611@gmail.com>
2022-09-11 14:57:44 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//for security reason only variable starts with REACT_APP_ 
+
 var some_environment_variable = "Some Variable";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +10,3 @@ root.render(
     <App env_var={some_environment_variable}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
