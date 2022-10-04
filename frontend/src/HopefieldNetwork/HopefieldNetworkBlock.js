e22c62ba3a3a31e6f83d6e84a0da49be418124ae
張天慈 <woodychang0611@gmail.com>
2022-10-04 23:03:10 +0800

    update click

--- frontend/src/HopefieldNetwork/HopefieldNetworkBlock.js
+++ frontend/src/HopefieldNetwork/HopefieldNetworkBlock.js
@@ -1,41 +1,66 @@
 import React from 'react';
 import "./HopefieldNetwork.css"
 
-function NodeBlock(index, value, config) {
+function NodeBlock(index, value, config, click) {
     let w = config.width
     let x = index % w
     let y = Math.floor(index / w)
     let fillColor = (value == 0) ? 'black' : 'white'
-    return <rect x={x} y={y} width="1" height="1" fill={fillColor} />
+    return <rect key={index} x={x} y={y} width="1" height="1" fill={fillColor}
+        onClick={
+            () => {
+                click(x, y)
+            }
+        }
+    />
 }
 
 
 class HopefieldNetworkBlock extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            dummy: "dummy"
-        }
-    }
-
-    render() {
 
         var config = {
-            width: 10,
-            height: 10
-        }
+            width: 5,
+            height: 5
+        };
 
         var data = [];
         for (var i = 0; i < config.width * config.height; i++) {
             data.push(Math.round(Math.random()))
         }
+        this.state = {
+            data: data,
+            config: config,
+            message: "message"
+        }
+
+
+
+    }
+
+
+    render() {
+        var onClick = (x, y) => {
+            let w = this.state.config.width
+
+            let data = this.state.data
+            data[w * y + x] = data[w * y + x] ? 0 : 1
+            this.setState({
+                message: `(${x},${y})`,
+                data: data
+            });
+        }
 
         return (
-            <svg className='HopefieldNetworkBlock' viewBox={`0 0 ${config.width} ${config.height}`}
-                preserveAspectRatio="xMidYMid meet">
-                <rect x="0" y="0" width={config.width} height={config.height} fill='darkblue' />
-                {data.map((v, i) => NodeBlock(i, v, config))}
-            </svg>
+            <div className='HopefieldNetworkBlock' >
+                <svg viewBox={`0 0 ${this.state.config.width} ${this.state.config.height}`}
+                    preserveAspectRatio="xMidYMid meet">
+                    <rect x="0" y="0" width={this.state.config.width} height={this.state.config.height} fill='darkblue' />
+                    {this.state.data.map((v, i) => NodeBlock(i, v, this.state.config, onClick))}
+                </svg>
+                <h6>{this.state.message}</h6>
+            </div>
         )
     }
 }
