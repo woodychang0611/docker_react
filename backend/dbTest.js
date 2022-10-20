518b363063ad2919d2bf4914fe8223c7aec99e2f
張天慈 <woodychang0611@gmail.com>
2022-10-20 22:27:50 +0800

    update

--- backend/dbTest.js
+++ backend/dbTest.js
@@ -5,21 +5,24 @@ const uri = "mongodb://root:example@127.0.0.1:27017";
 const client = new MongoClient(uri);
 
 async function run() {
-    try {
+  try {
 
-      let data = {
-        my_data:"my data value",
-        current_date:new Date()
-      }
-      // Establish and verify connection
-      db = await client.db("admin").command({listDatabases:1})
-      console.log(db)
-      await client.db("test_db").collection('test_collection').insertOne(data)
-      result = await client.db("test_db").collection('test_collection').findOne()
-      console.log(result)
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
+    let data = {
+      my_data: "my data value",
+      current_date: new Date()
     }
+    // Establish and verify connection
+    dbs = (await client.db("admin").command({ listDatabases: 1 })).databases
+    dbs.forEach(
+      (db)=>console.log(db.name)
+    )
+
+    await client.db("test_db").collection('test_collection').insertOne(data)
+    result = await client.db("test_db").collection('test_collection').findOne()
+    console.log(result)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
+}
 run().catch(console.dir);
