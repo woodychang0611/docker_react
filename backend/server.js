08bb927044326b035b1e1b7d541e629a5ee47bf0
張天慈 <woodychang0611@gmail.com>
2023-06-24 22:17:48 +0800

    update for post in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
- var express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser')
 var cors = require('cors');
 var app = express();
+
+// create application/json parser
+app.use(bodyParser.json())
 app.use(cors());
 
 let ServerPort = process.env.ServerPort
@@ -8,9 +12,18 @@ console.log(ServerPort)
 
 app.get('/date', function(req, res) {
   date = new Date()
-  res.json({ date: date});
+  console.log(`get date: ${date}`)
+  res.json({date: date});
 });
 
+
+app.post('/get_value', function(req, res) {
+  date = new Date()
+  console.log(req.body)
+  console.log(req.body.name)
+  console.log(`get value`)
+  res.json({status: 'OK'});
+});
 
 app.get('/info', function(req, res) {
   res.json({ info: process.env});
