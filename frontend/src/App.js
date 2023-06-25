b9a84b3a98e8ae36ec03f23df8529ca456fde545
張天慈 <woodychang0611@gmail.com>
2023-06-25 22:14:53 +0800

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,55 +2,76 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import HopefieldNetworkBlock from "./HopefieldNetwork/HopefieldNetworkBlock"
-import * as d3 from 'd3';
+import UserList from "./UserList/UserList"
 
-function resolveAfter2Seconds(x) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(x);
-    }, 2000);
-  });
-}
-async function setData(x){
-  return await resolveAfter2Seconds(x)
-}
 
 class App extends React.Component {
 
-    constructor(props){
-      super(props)
-      this.state={
-        var:this.props.var,
-        server_config:this.props.server_config
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      var: this.props.var,
+      userData: [],
+      data: "old data",
+      server_config: this.props.server_config
+    }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log(this.state.server_config)
     let address = `${this.state.server_config.SERVER_URL}/GET/date/`
+
+    setTimeout(() => {
+      let userData =
+        [
+          { name: "Bob", value: "Bob's value" },
+          { name: "Tim", value: "Tim's value" },
+          { name: "Jean", value: "Jean's value" }
+        ]
+      this.setState({ userData: userData });
+    }, 1000);
+
     fetch(address)
-    .then((response) => response.json())
-    .then(
-      s=>{
-        this.setState({
-          var:s.date}
+      .then((response) => {
+        console.log('aaaaa')
+        if (response.ok){
+          throw new Error ('response not ok')
+        }
+        response.json()
+      })
+      .then(
+        s => {
+          this.setState({
+            var: s.date
+          }
           )
-      }
-    )
+        }
+      ).catch(error => {
+        // Handle error
+        s => {
+          this.setState({
+            var: "not found"
+          }
+          )
+        }
+      })
+
   }
 
   render() {
 
+    let userData = this.state.userData
     return (
       <div className="App">
-      <header className="App-header">
-        <h3>Variable: {this.state.var}</h3>
-        <HopefieldNetworkBlock/>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+        <header className="App-header">
+          <UserList userData={userData} />
+          <h3>Variable: {this.state.var}</h3>
+          <HopefieldNetworkBlock />
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
     );
-}
+  }
 
 }
 
---
+++ frontend/src/UserList/UserList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+class UserComponent extends React.Component {
+    render() {
+        const { user } = this.props;
+        return (
+            <div>
+                {user.name}
+                <input type='text' value={user.value} onChange={()=>{}} />
+            </div>
+        )
+    }
+}
+
+class UserList extends React.Component {
+
+    render() {
+        const { userData } = this.props;
+        const childComponent = userData.map(user => (
+            <UserComponent key={user.name} user={user} />
+        ))
+
+        return (
+            <div>
+                <h3>User List</h3>
+                <div>{childComponent}</div>
+                <h3>------</h3>
+            </div>
+        )
+    }
+}
+
+export default UserList;
