27fb367574371c17d17b793edd235879fa23c10e
張天慈 <woodychang0611@gmail.com>
2023-07-23 22:29:53 +0800

    update

--- frontend/src/UserList/UserList.js
+++ frontend/src/UserList/UserList.js
@@ -4,12 +4,14 @@ import React from 'react';
 class UserComponent extends React.Component {
     render() {
         const { user } = this.props;
+
         return (
             <div>
                 {user.name}
-                <input type='text' value={user.value} onChange={()=>{}} />
+                <input type='text' value={user.value} onChange={() => { }} />
             </div>
         )
+
     }
 }
 
@@ -17,17 +19,22 @@ class UserList extends React.Component {
 
     render() {
         const { userData } = this.props;
-        const childComponent = userData.map(user => (
-            <UserComponent key={user.name} user={user} />
-        ))
 
-        return (
-            <div>
-                <h3>User List</h3>
-                <div>{childComponent}</div>
-                <h3>------</h3>
-            </div>
-        )
+        if (userData) {
+
+            const childComponent = userData.map(user => (
+                <UserComponent key={user.name} user={user} />
+            ))
+            return (
+                <div>
+                    <h3>User List</h3>
+                    <div>{childComponent}</div>
+                    <h3>------</h3>
+                </div>
+            )
+        } else {
+            return (<div>Loading</div>)
+        }
     }
 }
 
